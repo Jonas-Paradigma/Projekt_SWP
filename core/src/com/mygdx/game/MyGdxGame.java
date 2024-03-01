58909648db84d83924ea411ac6dfcede9e8dff45
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2024-03-01 09:10:34 +0100

    Test

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,18 +1,42 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+
+
+
+
+	private TextureAtlas atlas;
+	private Animation<TextureRegion> animation;
+	float elapsedTime = 0.1f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("images/badlogic.jpg");
+
+
+		//Sprite Sheet animation
+		atlas = new TextureAtlas(Gdx.files.internal("animation/santa_idle.atlas"));
+		Array<TextureAtlas.AtlasRegion> frames = atlas.findRegions("Armature_Idle");
+		animation = new Animation<>(0.01f, frames, Animation.PlayMode.LOOP);
+
+		atlas = new TextureAtlas(Gdx.files.internal("animation/santa_idle.atlas"));
+
+
+
+		img = new Texture("images/Background1.png");
 	}
 
 	@Override
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,19 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
+
+
+
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Projekt_SWP");
+		config.setWindowedMode(1000, 300);
+		config.setTitle("JetPack_Joyride");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
