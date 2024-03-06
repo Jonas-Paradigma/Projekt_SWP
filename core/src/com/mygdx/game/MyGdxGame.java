fc6d96c28073129435a1eea9e0deba61394d9ea4
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2024-03-06 09:34:38 +0100

    Test

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,12 +2,12 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
@@ -15,7 +15,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Texture background;
 	private Texture playerTexture;
 	private Vector2 playerPosition;
-	private boolean isPlayerRunning;
+	private boolean isPlayerFlying;
+	private float backgroundScrollSpeed;
 
 	@Override
 	public void create() {
@@ -26,8 +27,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		background = new Texture("images/Background1.png"); // Lade das Hintergrundbild
 		playerTexture = new Texture("images/0.png"); // Lade das Spielerbild
-		playerPosition = new Vector2(w / 2, h / 2); // Setze die Anfangsposition des Spielers in die Mitte des Bildschirms
-		isPlayerRunning = false;
+		playerPosition = new Vector2(w / 2 - playerTexture.getWidth() / 2, h / 2 - playerTexture.getHeight() / 2); // Setze die Anfangsposition des Spielers in die Mitte des Bildschirms
+		isPlayerFlying = false;
+		backgroundScrollSpeed = 2; // Hintergrundscrollgeschwindigkeit
 
 		// Setze die Kamera auf die Bildschirmgröße
 		camera.setToOrtho(false, w, h);
@@ -39,9 +41,14 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// Bewege den Spieler, wenn er läuft
-		if (isPlayerRunning) {
-			playerPosition.x += 2; // Beispielbewegung nach rechts
+		// Bewege den Hintergrund
+		backgroundScrollSpeed += Gdx.graphics.getDeltaTime(); // Aktualisiere die Scrollgeschwindigkeit
+		float backgroundOffsetX = backgroundScrollSpeed * 100; // Berechne den Hintergrundversatz basierend auf der Geschwindigkeit
+		backgroundOffsetX %= background.getWidth(); // Stelle sicher, dass der Hintergrund wiederholt wird
+
+		// Bewege den Spieler, wenn er fliegt
+		if (isPlayerFlying) {
+			playerPosition.y += 2; // Beispielbewegung nach oben
 		}
 
 		// Fokussiere die Kamera auf den Spieler
@@ -51,17 +58,23 @@ public class MyGdxGame extends ApplicationAdapter {
 		// Zeichne das Hintergrundbild
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// Zeichne den Hintergrund mehrmals nebeneinander, um eine nahtlose Wiederholung zu erzeugen
+		for (int i = 0; i < 2; i++) {
+			batch.draw(background, i * background.getWidth() - backgroundOffsetX, 0);
+		}
 		batch.draw(playerTexture, playerPosition.x, playerPosition.y);
 		batch.end();
 
 		// Überprüfe Benutzerinteraktion
-		if (Gdx.input.justTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos); // Umrechnung von Bildschirmkoordinaten zu Spielkoordinaten
-			// Hier kannst du deine Logik für Benutzerinteraktion implementieren
-			isPlayerRunning = true; // Starte die Spielerbewegung, wenn der Bildschirm berührt wird
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+			if (!isPlayerFlying) {
+				isPlayerFlying = true; // Starte das Fliegen, wenn die Leertaste gedrückt wird
+			}
+		}
+
+		// Stoppe das Fliegen, wenn der Spieler den oberen Bildschirmrand erreicht
+		if (playerPosition.y >= Gdx.graphics.getHeight() - playerTexture.getHeight()) {
+			isPlayerFlying = false;
 		}
 	}
 
